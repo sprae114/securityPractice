1ebd231682fa1cbda68dc9a254a665fc748f2f72
Yohan <sprae114@naver.com>
2023-08-21 17:45:43 +0900

    ch01 초기설정

---
+++ ch01basic-test/src/main/java/com/example/ch01basictest/Ch01basicTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch01basictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch01basicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch01basicTestApplication.class, args);
+    }
+
+}
