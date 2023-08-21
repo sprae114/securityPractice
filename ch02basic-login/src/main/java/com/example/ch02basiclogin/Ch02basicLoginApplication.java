488d82ac6b52f4141f930530c9575cdd3483dbaa
Yohan <sprae114@naver.com>
2023-08-22 00:04:15 +0900

    ch02 초기 설정

---
+++ ch02basic-login/src/main/java/com/example/ch02basiclogin/Ch02basicLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch02basiclogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02basicLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02basicLoginApplication.class, args);
+    }
+
+}
