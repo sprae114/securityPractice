f76c03ea433fd7b52690540a5316d30de49e79d3
Yohan <sprae114@naver.com>
2023-08-26 05:54:05 +0900

    ch04 초기 설정 및 BasicAuthenticationFilter 추가

---
+++ ch04basic-authentication-test/src/main/java/com/example/ch04basicauthenticationtest/Ch04basicAuthenticationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch04basicauthenticationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch04basicAuthenticationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch04basicAuthenticationTestApplication.class, args);
+    }
+
+}
---
+++ ch04basic-authentication-test/src/test/java/com/example/ch04basicauthenticationtest/Ch04basicAuthenticationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch04basicauthenticationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch04basicAuthenticationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
