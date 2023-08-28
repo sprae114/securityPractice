19fe1844d6fe401022a4751802ab5a9e00f375e7
yohan <dygks34@gmail.com>
2023-08-28 19:35:54 +0900

    common 모듈 만들기

---
+++ common-student/src/main/java/com/example/commonstudent/CommonStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.commonstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonStudentApplication.class, args);
+    }
+
+}
---
+++ common-student/src/test/java/com/example/commonstudent/CommonStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commonstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
