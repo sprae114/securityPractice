1d786fb164276a747452541abd96dbc6e8ec3e6c
yohan <dygks34@gmail.com>
2023-08-29 03:03:07 +0900

    ch06 초기설정

---
+++ ch06login-userDetails/src/main/java/com/example/ch06loginuserdetails/Ch06loginUserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch06loginuserdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch06loginUserDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch06loginUserDetailsApplication.class, args);
+    }
+
+}
---
+++ ch06login-userDetails/src/test/java/com/example/ch06loginuserdetails/Ch06loginUserDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch06loginuserdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch06loginUserDetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
